c09ff07bd79d99ab019b13884d4f0b06bee14422
wbellan <wbellanger@gmail.com>
2016-04-20 10:22:04 -0500

    starting over

--- README.md
+++
@@ -1,2 +0,0 @@
-# proprietaire
-Propriétaire
--- src/frontend/app/boot.ts
+++
@@ -1,6 +0,0 @@
-import {enableProdMode} from 'angular2/core';
-import {bootstrap} from 'angular2/platform/browser';
-import {HomeComponent} from './home/home.component';
-
-enableProdMode();
-bootstrap(HomeComponent, []);
