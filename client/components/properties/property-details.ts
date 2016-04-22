8bd1ef2670451266bddefc819b55d90aeea90097
wbellan <wbellanger@gmail.com>
2016-04-22 14:39:55 -0500

    added link from prop details to prop listing

--- client/components/properties/property-details.ts
+++ client/components/properties/property-details.ts
@@ -1,12 +1,16 @@
 import {Component} from 'angular2/core';
 import {RouteParams} from 'angular2/router';
+import {RouterLink} from 'angular2/router';
 import {Properties} from '../../../collections/properties';
 
 @Component({
   selector: 'property-details',
   templateUrl: '/client/components/properties/property-details.html',
+  directives: [RouterLink]
 })
 export class PropertyDetails {
+  property: Object;
+
   constructor(params: RouteParams) {
     var propertyId = params.get('propertyId');
     this.property = Properties.findOne(propertyId);
