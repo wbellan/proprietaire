d5509c92aa0687f3e7d36d06478911ae9f17edf5
wbellang <wbellang@gmail.com>
2017-08-02 15:49:31 -0500

    Added dockerfile and some template setup

--- gatekeeper/server.js
+++ gatekeeper/server.js
@@ -1,29 +1,43 @@
 const express = require('express');
-const app = express();
-
 const port = process.env.PORT || 8080;
 const mongoose = require('mongoose');
 const passport = require('passport');
-const flash    = require('connect-flash');
-const uid = require('uid-safe')
-
-const morgan       = require('morgan');
+const flash = require('connect-flash');
+const uid = require('uid-safe');
+const hbs = require('express-hbs');
+const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
-const session      = require('express-session');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const path = require('path');
 
 const configDatabase = require('./config/database.js');
-mongoose.createConnection(configDatabase.url, { server: { poolSize: 4 }});
+mongoose.createConnection(configDatabase.url, {
+  server: {
+    poolSize: 4
+  }
+});
 
 require('./config/passport')(passport);
+
+var app = express();
+
+app.set('view engine', 'hbs');
+// configure the view engine
+app.engine('hbs', hbs.express4({
+  defaultLayout: __dirname + '/views/layouts/main.hbs',
+  partialsDir: __dirname + '/views/partials',
+  layoutsDir: __dirname + '/views/layouts'
+}));
+// configure views path
+app.set('views', path.join(__dirname, '/views'));
 
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.set('view engine', 'ejs');
-app.set('trust proxy', 1) // trust first proxy
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 var sess = {
   secret: 'mysupersecret',
@@ -34,12 +48,10 @@ var sess = {
     return uid.sync(18);
   }
 }
-
 if (app.get('env') === 'production') {
   app.set('trust proxy', 1) // trust first proxy
   sess.cookie.secure = true // serve secure cookies
 }
-
 app.use(session(sess));
 
 app.use(passport.initialize());
@@ -49,4 +61,4 @@ app.use(flash());
 require('./app/routes.js')(app, passport);
 
 app.listen(port);
-console.log(`The Propriétaire portal is now listening on port ${port}.`);
+console.log(`The Propriétaire gatekeeper is now listening on port ${port}.`);
